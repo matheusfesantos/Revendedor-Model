6088c1b84bc0a4a95c1a3c29614d28c7fa6d74da
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-11-26 00:15:20 -0300

    Ajustes de responsividade

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
-setTimeout(function() {
-    window.location.href = '/src/index2.html';
-}, 4000);
+console.log("Trocnaod tela")
+try{
+    setTimeout(function() {
+        window.location.href = '/src/index2.html';
+    }, 4000);
+    console.log("Tela Carregada")
+} catch{
+    console.log("Algum erro existente")
+}
+
